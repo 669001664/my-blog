0695a5075ae51a62126806dfdf921357e35ad6c8
moonlight <669001664@qq.com>
2021-01-23 17:16:57 +0800

    项目结束

--- myBlog/app.js
+++ myBlog/app.js
@@ -6,10 +6,13 @@ const path = require("path")
 const staticPath = require('koa-static')
 
 const bodyParser = require('koa-bodyparser')
+const session = require("koa-session");
 
 const app = new Koa()
 
-const router = require("./routes")
+const blog = require("./routes/blog")
+const user = require("./routes/user")
+
 
 app.use(bodyParser())
 //加载模板引擎
@@ -18,12 +21,31 @@ app.use(
         extension: "ejs"
     })
 )
-
 app.use(staticPath(
     path.join(__dirname, './public')
 ))
 
 
-app.use(router.routes()).use(router.allowedMethods());
+app.keys = ["myblog_session_key$$"];
+app.use(session(app));
+
+
+app.use(async (ctx, next) => {
+    if (ctx.url == "/login" || ctx.url == "/regist") {
+      await next();
+    } else {
+      let loginUser = ctx.session.loginUser;
+      if (loginUser) {
+        await next();
+      } else {
+        ctx.redirect("/login");
+      }
+    }
+  });
+
+
+
+app.use(user.routes()).use(user.allowedMethods());
+app.use(blog.routes()).use(blog.allowedMethods());
 app.listen(3000)
 console.log('[demo] start-quick is starting at port 3000')
